029155d6b200cc0be452bcddd08a6794abdd66d0
Alexandre Heiden <alexandreheiden@hotmail.com>
2021-05-17 23:47:35 -0300

    results for top 10 stocks

--- sentiment_analysis/sentiment_analysis.py
+++ sentiment_analysis/sentiment_analysis.py
@@ -7,31 +7,35 @@ def avg(list):
   else:
     return sum(list) / len(list)
 
-tickers = ['GOOG']
+years = ['2019', '2020']
+tickers = ['AAPL', 'MSFT', 'AMZN', 'GOOG', 'FB', 'BRK-B', 'V', 'WMT', 'JNJ', 'PG']
+# tickers = ['GOOG']
 
-for ticker in tickers:
-  df = pd.read_csv('news/{}.csv'.format(ticker), delimiter=';')
-  headlines = df['Headline'].values
-  dates = [x for x in zip(df['Day'], df['Month'])]
+for year in years:
+  for ticker in tickers:
+    print(ticker, year)
+    df = pd.read_csv('news/{}/{}.csv'.format(year, ticker), delimiter=';')
+    headlines = df['Headline'].values
+    dates = [x for x in zip(df['Day'], df['Month'])]
 
-  # using 1-31 for day and 1-12 for month to be clearer
-  sentiments = [[[] for x in range(13)] for y in range(32)]
-  analyzer = SentimentIntensityAnalyzer()
+    # using 1-31 for day and 1-12 for month to be clearer
+    sentiments = [[[] for x in range(13)] for y in range(32)]
+    analyzer = SentimentIntensityAnalyzer()
 
-  for pair in zip(headlines, dates):
-    headline, date = pair
-    vader_sentiment_scores = analyzer.polarity_scores(headline)
-    sentiments[date[0]][date[1]].append(vader_sentiment_scores['compound'])
+    for pair in zip(headlines, dates):
+      headline, date = pair
+      vader_sentiment_scores = analyzer.polarity_scores(headline)
+      sentiments[date[0]][date[1]].append(vader_sentiment_scores['compound'])
 
-  df = pd.read_csv('stocks/{}_2020.csv'.format(ticker))
-  trading_dates = df['Date'].values
+    df = pd.read_csv('stocks/{}/{}.csv'.format(year, ticker))
+    trading_dates = df['Date'].values
 
-  sentiment_column = []
-  for date in trading_dates:
-    year, month, day = list(map(int, date.split('-')))
-    sentiment_column.append(avg(sentiments[day][month]))
+    sentiment_column = []
+    for date in trading_dates:
+      year, month, day = list(map(int, date.split('-')))
+      sentiment_column.append(avg(sentiments[day][month]))
 
-  df['Sentiment'] = sentiment_column
+    df['Sentiment'] = sentiment_column
 
-  with open('results/{}.csv'.format(ticker), 'w') as file:
-    file.write(df.to_string())
+    with open('results/{}/{}.csv'.format(year, ticker), 'w') as file:
+      file.write(df.to_string())
